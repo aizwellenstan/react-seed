a87f6c0f172c41e49511d90a693d11c88f003a59
aizwellenstan <aizwellenstan@gmail.com>
2020-02-06 11:31:31 +0900

    nothing

--- client/src/views/assets/index.tsx
+++ client/src/views/assets/index.tsx
@@ -2,18 +2,39 @@ import * as React from 'react';
 // import { TimetableContainer } from 'client/src/containers/timetable-container';
 import { SidebarContainer } from 'client/src/containers/sidebar-container';
 import { Page } from 'client/src/components/page';
-
 import { Redirect } from 'react-router-dom';
+import { styled } from 'client/src/styles';
+import { sidebarWidth } from 'client/src/styles/constants';
+
+import Template from './components/template.html';
+var htmlDoc = { __html: Template };
 
 export const Assets = React.memo(() => {
+  const Wrapper = styled.div`
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    margin-left: 0;
+    overflow-y: scroll;
+    -webkit-overflow-scrolling: touch;
+
+    @media screen and (min-width: 700px) {
+      width: calc(100% - ${sidebarWidth}px);
+      margin-left: ${sidebarWidth}px;
+    }
+  `;
+
   if (localStorage.getItem('login') !== 'true') {
     return <Redirect exact from="/admin" to="/login" />;
   }
   return (
     <Page>
-      Admin
       <SidebarContainer />
-      {/* <TimetableContainer /> */}
+      <Wrapper>
+        <div dangerouslySetInnerHTML={htmlDoc} />
+      </Wrapper>
     </Page>
   );
 });
--- server/datasources/index.ts
+++ server/datasources/index.ts
@@ -1,8 +1,8 @@
 import * as express from 'express';
-import * as request from 'request';
-// import { RESOURCE_HOST, RESOURCE_PROTOCOL } from '../config';
+// import * as request from 'request';
+// // import { RESOURCE_HOST, RESOURCE_PROTOCOL } from '../config';
 
-import { RESOURCE_PROTOCOL } from '../config';
+// import { RESOURCE_PROTOCOL } from '../config';
 
 const router = express.Router();
 
@@ -11,18 +11,5 @@ const router = express.Router();
 //   const boundPath = `${RESOURCE_PROTOCOL}://${RESOURCE_HOST}${req.path}`;
 //   req.pipe(request(boundPath)).pipe(res);
 // });
-
-router.use('/railsapi', (req, res) => {
-  const boundPath = `http://127.0.0.1:4000//${req.path}`;
-  // const boundPath = `${RESOURCE_PROTOCOL}://product.nadi3docms.com/api${req.path}`;
-  req.pipe(request(boundPath)).pipe(res);
-});
-
-router.use('/alarmapi', (req, res) => {
-  const boundPath = `${RESOURCE_PROTOCOL}://product.nadi3docms.com/Siemens${
-    req.path
-  }`;
-  req.pipe(request(boundPath)).pipe(res);
-});
 
 export const dataSources = router;
