180347f2c60707b314fe6ca975b565555a246745
aizwellenstan <aizwellenstan@gmail.com>
2020-02-13 18:36:57 +0900

    fix

---
+++ dist/datasources/index.js
@@ -0,0 +1,30 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var express = require("express");
+// import * as request from 'request';
+// import * as fetch from 'node-fetch'
+// // import { RESOURCE_HOST, RESOURCE_PROTOCOL } from '../config';
+// import { RESOURCE_PROTOCOL } from '../config';
+var router = express.Router();
+// Bind /api/* to original API server
+// router.use('/api', (req, res) => {
+//   const boundPath = `${RESOURCE_PROTOCOL}://${RESOURCE_HOST}${req.path}`;
+//   req.pipe(request(boundPath)).pipe(res);
+// });
+// router.use('/assetapi', async (req, res) => {
+//   // const boundPath: string = `${RESOURCE_PROTOCOL}://${RESOURCE_HOST}${req.path}`;
+//   // const boundPath: string = `https://api.github.com${req.path}`
+//   var query = ''
+//   for (let key in req.query) {
+//     query += `${key}=${req.query[key]}&`
+//   }
+//   const boundPath: string = `http://127.0.0.1:7004${req.path}?${query}`
+//   const response = await fetch(boundPath, {
+//     headers: {
+//       username: `${req.header('username')}`
+//     }
+//   })
+//   const json = await response.json()
+//   res.json(json)
+// });
+exports.dataSources = router;
