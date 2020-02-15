3738218218cd71ddf9cdf8f9b63a1fd73f889ce1
aizwellenstan <aizwellenstan@gmail.com>
2020-02-15 19:51:42 +0900

    fix

--- client/src/views/home/index.tsx
+++ client/src/views/home/index.tsx
@@ -25,45 +25,7 @@ export const Home = React.memo(() => {
   }
   return (
     <>
-      <Helmet>
-        <script
-          type="text/javascript"
-          src="https://product.nadi3docms.com/nadidemo3/assets/js/jquery-3.2.1.min.js"
-        />
-        <script
-          type="text/javascript"
-          src="https://product.nadi3docms.com/nadidemo3/assets/js/bootstrap.min.js"
-        />
-        <script
-          type="text/javascript"
-          src="https://product.nadi3docms.com/nadidemo3/assets/js/jquery.marquee.min.js"
-        />
-        <script
-          type="text/javascript"
-          src="https://product.nadi3docms.com/nadidemo3/assets/js/owl.carousel.min.js"
-        />
-        <script
-          type="text/javascript"
-          src="https://product.nadi3docms.com/nadidemo3/assets/js/main.js"
-          defer
-        />
-        <link
-          rel="stylesheet"
-          href="https://product.nadi3docms.com/nadidemo3/assets/css/font-awesome.min.css"
-        />
-        <link
-          rel="stylesheet"
-          href="https://product.nadi3docms.com/nadidemo3/assets/css/owl.carousel.css"
-        />
-        <link
-          rel="stylesheet"
-          href="https://product.nadi3docms.com/nadidemo3/assets/css/style.css"
-        />
-        <link
-          rel="stylesheet"
-          href="https://product.nadi3docms.com/nadidemo3/assets/css/animate.css"
-        />
-      </Helmet>
+      <Helmet />
       <Page>
         <Wrapper>
           <div dangerouslySetInnerHTML={htmlDoc} />
